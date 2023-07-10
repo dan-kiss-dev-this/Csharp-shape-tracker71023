46b63bb691f149a1528c55ce0f5f9a9823f52068
Daniel Kiss <flowfast47@gmail.com>
2023-07-10 14:14:21 -0700

    Add testing and Rectangle in TDD format
    Co-authored-by: Michael Carroll mcarroll138@gmail.com

---
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -0,0 +1,49 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShapeTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace ShapeTracker.Tests
+{
+  // [TestClass]
+  // public class RectangleTests : IDisposable
+  // {
+  //   public void Dispose()
+  //   {
+  //     Rectangle.ClearAll()
+  //   }
+  // }
+
+  //get a side
+  [TestClass]
+  public class RectangleTests
+  {
+    [TestMethod]
+    public void RectangleContructor_MakeARectangle_Rectangle()
+    {
+
+      Rectangle testRectangle = new Rectangle(2, 3);
+      // expect(side1).ToEqual(testRectangle.Side1);
+      Assert.AreEqual(typeof(Rectangle), testRectangle.GetType());
+    }
+
+    [TestMethod]
+    public void Side1_GetAndSetSide_Int()
+    {
+      int side1 = 3;
+      int side2 = 4;
+      Rectangle newRectangle = new Rectangle(side1, side2);
+      Assert.AreEqual(side1, newRectangle.Side1);
+    }
+
+    [TestMethod]
+    public void Side2_GetAndSetSide_Int()
+    {
+      int side1 = 3;
+      int side2 = 4;
+      Rectangle newRectangle = new Rectangle(side1, side2);
+      Assert.AreEqual(side2, newRectangle.Side2);
+    }
+
+  }
+}
---
+++ ShapeTracker/Models/Rectangle.cs
@@ -0,0 +1,16 @@
+namespace ShapeTracker.Models
+{
+  public class Rectangle
+  {
+    public int Side1 { get; set; }
+    public int Side2 { get; set; }
+
+    public Rectangle(int side1, int side2)
+    {
+      Side1 = side1;
+      Side2 = side2;
+    }
+    // side1, side2, instances, GetInstances
+    //CalculateArea function
+  }
+}
